019ede10927d3ba7a0e95abcbcf422eb089d11be
taccianka <tacciankastudy@gmail.com>
2022-01-27 03:14:09 +0300

    feat: solve morse task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,60 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  let arraySymbols = [];
+  let symbolStringArray = '';
+  for (let i = 0; i < expr.length; i = i + 10) {
+    let symbolBinary = expr.substring(i, i + 10);
+    if (symbolBinary.includes('*')) {
+      arraySymbols.push(' ');
+      continue;
+    }
+    symbolBinary = symbolBinary.replace(/11/g, '-');
+    symbolBinary = symbolBinary.replace(/10/g, '.');
+    symbolBinary = symbolBinary.replace(/00/g, '');
+    arraySymbols.push(MORSE_TABLE[symbolBinary]);
+
+  }
+  return arraySymbols.join('');
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
